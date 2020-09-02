a2125ccfeee3a8053e6b5b24a1167d00b009c212
jmcfadd <justinmcfadd4@gmail.com>
2020-09-02 12:19:08 -0400

    Sign Up Page

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,36 @@ const theme = createMuiTheme({
             contrastText: '#fff'
         }
     },
-    typography: {
-        useNextVariants: true,
+    spreadThis: {
+        typography: {
+            useNextVariants: true
+        },
+        form: {
+            textAlign: "center"
+        },
+        image: {
+            margin: "10px auto 10px auto"
+        },
+        pageTitle: {
+            margin: "10px auto 10px auto"
+        },
+        textField: {
+            margin: "10px auto 10px auto"
+        },
+        button: {
+            marginTop: 20,
+            position: "relative"
+        },
+        customError: {
+            color: "red",
+            fontSize: "0.8rem",
+            marginTop: 5
+        },
+        progress: {
+            position: "absolute"
+        }
     }
-})
+});
 
 function App() {
     return (
